1a52ebafaf5b8187cb7d2eac677e2174a98ac9b7
Brandon Parker <newboots@gmail.com>
2019-03-14 15:52:02 -0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -19,8 +19,7 @@ handleClick(event) {
   var options = ['ROCK','PAPER','SCISSORS'];
   var computerchoice = options[Math.floor(Math.random()*options.length)];
 
-  console.log('Computer picks ' + computerchoice)
-  console.log('User picks ' + userchoiceSelected)
+  
 
   const gameresult = "You chose  " + userchoiceSelected + " Computer chose  " + computerchoice + "";
   const nextgameresults = this.state.gameresults.concat(this.state.currentgameresult);
@@ -36,18 +35,16 @@ handleClick(event) {
     return (
 
       <div className="App">
-        <div className="App-header">
         <header>
           <h1> Welcome to Rock Paper Scissors</h1>
           <h2>Click an Icon below</h2>
         </header>
-        </div>
         <div>
           <br />
           <section className="pics">
-            <img src="https://visualpharm.com/assets/4/Rock-595b40b75ba036ed117d8450.svg"onClick={this.handleClick}id='ROCK'></img>
-            <img src="https://visualpharm.com/assets/195/Toilet%20Paper-595b40b85ba036ed117dbc6d.svg"onClick={this.handleClick}id='PAPER'></img>
-            <img src="https://visualpharm.com/assets/823/Surgical%20Scissors-595b40b75ba036ed117d723c.svg"onClick={this.handleClick}id='SCISSORS'></img>
+            <img src="https://visualpharm.com/assets/4/Rock-595b40b75ba036ed117d8450.svg" onClick={this.handleClick}id='ROCK'></img>
+            <img src="https://visualpharm.com/assets/195/Toilet%20Paper-595b40b85ba036ed117dbc6d.svg" onClick={this.handleClick}id='PAPER'></img>
+            <img src="https://visualpharm.com/assets/823/Surgical%20Scissors-595b40b75ba036ed117d723c.svg" onClick={this.handleClick}id='SCISSORS'></img>
           </section>
         </div>
             <br />
@@ -55,7 +52,6 @@ handleClick(event) {
             <br />
             <br />
             {this.state.currentgameresult} </h2>
-            <br />
             <br />
             <h2> Past Results: 
             {this.state.gameresults.map((todo,index) => <p key={index}>{todo}</p>)}</h2>
