4645f594157ffd43b6a850a38c63547263776438
Brandon Parker <newboots@gmail.com>
2019-03-14 14:55:36 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+constructor(props){
+  super(props);
+
+  this.state={
+    gameresults:[],
+    currentgameresult:""
+  };
+
+  this.handleClick = this.handleClick.bind(this);
+}
+
+handleClick(event) {
+  const userchoiceSelected = event.target.id;
+  var options = ['ROCK','PAPER','SCISSORS'];
+  var computerchoice = options[Math.floor(Math.random()*options.length)];
+
+  console.log('Computer picks ' + computerchoice)
+  console.log('User picks ' + userchoiceSelected)
+
+  const gameresult = "You chose  " + userchoiceSelected + " Computer chose  " + computerchoice + "";
+  const nextgameresults = this.state.gameresults.concat(this.state.currentgameresult);
+  
+  this.setState({
+    currentgameresult: gameresult,
+    gameresults: nextgameresults
+
+  });
+
+}
   render() {
     return (
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <div className="App-header">
+        <header>
+          <h1> Welcome to Rock Paper Scissors</h1>
+          <h2>Click an Icon below</h2>
         </header>
+        </div>
+        <div>
+          <br />
+          <section className="pics">
+            <img src="https://visualpharm.com/assets/4/Rock-595b40b75ba036ed117d8450.svg"onClick={this.handleClick}id='ROCK'></img>
+            <img src="https://visualpharm.com/assets/195/Toilet%20Paper-595b40b85ba036ed117dbc6d.svg"onClick={this.handleClick}id='PAPER'></img>
+            <img src="https://visualpharm.com/assets/823/Surgical%20Scissors-595b40b75ba036ed117d723c.svg"onClick={this.handleClick}id='SCISSORS'></img>
+          </section>
+        </div>
+            <br />
+            <h2> Current Results:
+            <br />
+            <br />
+            {this.state.currentgameresult} </h2>
+            <br />
+            <br />
+            <h2> Past Results: 
+            {this.state.gameresults.map((todo,index) => <p key={index}>{todo}</p>)}</h2>
       </div>
     );
   }
 }
-
 export default App;
